2dc01b88ca3865ceeb63bb52efe70406d2e95935
bersling <bersling@gmail.com>
2017-01-22 22:22:59 +0100

    fix

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ with open('mathjax-parser.js', 'r') as myfile:
 
 angularStart = "(function(window, angular) {\n";
 angularEnd = "  var parser = new MathjaxParser();\n" + \
-             "angular.module('MathjaxParser, []).service('MathjaxParser', parser);\n" + \
+             "angular.module('MathjaxParser, []).service('MathjaxParserService', parser);\n" + \
              "})(window, window.angular);";
 
 print(angularStart + contents + angularEnd);
