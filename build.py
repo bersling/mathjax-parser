c7a471ed41695bc2faf7d35ebfa720e5c085929c
bersling <bersling@gmail.com>
2017-01-22 22:38:20 +0100

    update build

--- build.py
+++ build.py
@@ -3,9 +3,10 @@ with open('mathjax-parser.js', 'r') as myfile:
     contents=myfile.read()
 
 angularStart = "(function(window, angular) {\n";
-angularEnd = "  var parser = new MathjaxParser();\n" + \
-             "angular.module('MathjaxParser, []).service('MathjaxParserService', parser);\n" + \
+angularEnd = "var parser = new MathjaxParser();\n" + \
+             "angular.module('MathjaxParser', []).service('MathjaxParserService', parser);\n" + \
              "})(window, window.angular);";
 
-print(angularStart + contents + angularEnd);
-
+f = open('omathjax-parser.js', 'w')
+f.write(angularStart + contents + angularEnd)  # python will convert \n to os.linesep
+f.close()  # you can omit in most cases as the destructor will call it
